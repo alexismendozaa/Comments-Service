c1e2a99bcae0a03e9258d0453da387caf4074844
alexismendozaa <afmendozaf@uce.edu.ec>
2025-06-27 19:59:15 -0500

    🧱 build: add solution files for each microservice

---
+++ add-comment-ms/add-comment-ms/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace add_comment_ms.Models
+{
+    public class Comment
+    {
+        public string Id { get; set; } // UUID
+        public string PostId { get; set; } // UUID
+        public string UserId { get; set; } // UUID
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ add-comment-ms/add-comment-ms/Models/CommentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace add_comment_ms.Models
+{
+    public class CommentDto
+    {
+        public string PostId { get; set; }
+        public string Text { get; set; }
+    }
+}
