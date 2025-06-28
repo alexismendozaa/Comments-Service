cd77674c8a43fb32d2f0b08c6a6d0e9d9aa55ffc
alexismendozaa <afmendozaf@uce.edu.ec>
2025-06-27 20:01:10 -0500

    🧪 test(edit): test comment update flow

---
+++ list-comment-ms/list-comment-ms/Services/DatabaseConfig.cs
@@ -0,0 +1,36 @@
+﻿using Npgsql;
+using Microsoft.Extensions.Configuration;
+
+public class DatabaseConfig
+{
+    private readonly IConfiguration _configuration;
+
+    public DatabaseConfig(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public NpgsqlConnection GetUsersConnection()
+    {
+        var host = Environment.GetEnvironmentVariable("DB_HOST");
+        var port = Environment.GetEnvironmentVariable("DB_PORT");
+        var db = Environment.GetEnvironmentVariable("DB_USERS");
+        var user = Environment.GetEnvironmentVariable("DB_USER");
+        var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+        var connectionString = $"Host={host};Port={port};Database={db};Username={user};Password={password}";
+        return new NpgsqlConnection(connectionString);
+    }
+
+    public NpgsqlConnection GetCommentsConnection()
+    {
+        var host = Environment.GetEnvironmentVariable("DB_HOST");
+        var port = Environment.GetEnvironmentVariable("DB_PORT");
+        var db = Environment.GetEnvironmentVariable("DB_COMMENTS");
+        var user = Environment.GetEnvironmentVariable("DB_USER");
+        var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+        var connectionString = $"Host={host};Port={port};Database={db};Username={user};Password={password}";
+        return new NpgsqlConnection(connectionString);
+    }
+}
