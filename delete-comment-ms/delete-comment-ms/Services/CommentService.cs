7856ada206897c060b294db9258475347c33d2b9
alexismendozaa <afmendozaf@uce.edu.ec>
2025-06-27 20:00:33 -0500

    ✨ feat(edit): implement update comment controller and service

---
+++ delete-comment-ms/delete-comment-ms/Services/CommentService.cs
@@ -0,0 +1,38 @@
+﻿using delete_comment_ms.Data;
+using delete_comment_ms.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace delete_comment_ms.Services
+{
+    public class CommentService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> DeleteCommentAsync(Guid commentId, Guid userId)
+        {
+            var comment = await _context.Comments
+                .FirstOrDefaultAsync(c => c.Id == commentId);
+
+            if (comment == null)
+            {
+                return false; // El comentario no se encontró
+            }
+
+            if (comment.UserId != userId)
+            {
+                return false; // El usuario no puede borrar este comentario
+            }
+
+            _context.Comments.Remove(comment);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
