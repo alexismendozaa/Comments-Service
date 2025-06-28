27a74218b3bc59e7785a8e62a74ffe78a7f0d4cc
alexismendozaa <afmendozaf@uce.edu.ec>
2025-06-27 20:01:39 -0500

    📦 build(delete): configure Dockerfile for delete service

---
+++ list-comment-ms/list-comment-ms/Models/Comment.cs
@@ -0,0 +1,8 @@
+﻿public class Comment
+{
+    public Guid Id { get; set; }
+    public string Text { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+}
